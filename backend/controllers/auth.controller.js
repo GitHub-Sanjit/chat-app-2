602af0541a9d1bd29434efdccfb64d3da3f8e31b
sanjit <sanjit.est14@gmail.com>
2026-03-22 22:15:14 +0600

    DB Setup and Signup Routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,59 @@
-export const signup = (req, res) => {
+import bcrypt from "bcryptjs";
+
+import User from "./../models/user.model.js";
+import generateTokenAndSetToken from "../utils/generateToken.js";
+
+export const signup = async (req, res) => {
   try {
-    const { fullName, userName, password, confirmPassword, gender } = req.body;
-  } catch (error) {}
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.staus(400).json({ error: "Password don't match" });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.staus(400).json({ error: "Username already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // https://avatar-placeholder.iran.liara.run/
+
+    // const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const boyProfilePic =
+      "https://pngtree.com/freepng/man-avatar-image-for-profile_13001882.html";
+    // const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+    const girlProfilePic =
+      "https://favpng.com/png_view/3d-woman-avatar-stylized-cartoon-woman-avatar-with-glasses-png/g0FutwYY";
+
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    if (newUser) {
+      generateTokenAndSetToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.staus(400).json({ error: "Invalid User Data" });
+    }
+  } catch (error) {
+    console.log("Error in Signup Controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export const login = (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  // Root route
-  res.send("Hello World");
-});
+app.use(express.json()); // To parse the incoming request with JSON payloads
 
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
+  connectToMongoDB();
   console.log(`Server is running on PORT: ${PORT}`);
 });
+
+// XnfLNPg3hZWuDJI5
+// 596812
