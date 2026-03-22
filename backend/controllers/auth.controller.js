c3f3375317f9e7b802384a5e3051be835a87d997
sanjit <sanjit.est14@gmail.com>
2026-03-22 18:49:35 +0600

    Backend Initial Setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+  res.send({ signup: "signup" });
+};
+
+export const login = (req, res) => {
+  res.send({ login: "login" });
+};
+
+export const logout = (req, res) => {
+  res.send({ logout: "logout" });
+};
